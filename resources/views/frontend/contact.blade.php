014cb89e8040176681c329c01ad5efab94f1d54a
mdmujibur741 <mdmujibur741@gmail.com>
2022-09-29 10:06:02 +0600

    contact some edit done

--- resources/views/frontend/contact.blade.php
+++ resources/views/frontend/contact.blade.php
@@ -16,36 +16,40 @@
                 @csrf
                 <div class="row justify-content-center">
                     
-                    <div class="col-sm-12 col-lg-4">
-                        <div class="mb-3">
-                            <label for="name"><b>নাম</b></label>
-                            <input type="text" name="name" class="form-control rounded" id="name" placeholder="নাম লিখুন">
-                        </div> 
+                    <div class="col-lg-6 contact_form px-5 py-3 bg-light shadow">
+                        <h3 class="shadow contact_title text-center py-2"><b>যোগাযোগ করুন</b></h3>
+                        <div class="d-flex justify-content-center mt-4">
+                            <div class="mb-3 flex-fill ">
+                                <label for="name"><b>নাম</b></label>
+                                <input type="text" name="name" class="form-control rounded" id="name" placeholder="নাম লিখুন">
+                            </div> 
+                            
+                       
                         
-                    </div>
-                     <div class="col-sm-12 col-lg-4">
-                        <div class="mb-3">
-                            <label for="email"><b>ইমেইল</b></label>
-                            <input type="text" name="email" class="form-control rounded" id="email" placeholder="ইমেইল লিখুন">
+                            <div class="mb-3 flex-fill ms-2">
+                                <label for="email"><b>ইমেইল</b></label>
+                                <input type="text" name="email" class="form-control rounded" id="email" placeholder="ইমেইল লিখুন">
+                            </div>
+                        
                         </div>
-                     </div>
-
-                     <div class="col-sm-12 col-lg-8">
-                        <div class="mb-3">
-                            <label for="phone"><b>মোবাইল</b></label>
-                            <input type="text" name="mobile" class="form-control rounded" id="phone" placeholder="মোবাইল নং লিখুন">
-                        </div>
-                     </div>
-
-                     <div class="col-sm-12 col-lg-8">
-                        <div class="mb-3">
-                            <label for="message"><b>মেসেজ</b></label>
-                             <textarea name="message" id="message" class="form-control rounded" rows="4"></textarea>
-                        </div>
-                     </div>
-                     <div class="mb-3 text-center">
-                         <input type="submit" value="সাবমিট" class="btn">
-                     </div>
+    
+                        
+                            <div class="mb-3">
+                                <label for="phone"><b>মোবাইল</b></label>
+                                <input type="text" name="mobile" class="form-control rounded" id="phone" placeholder="মোবাইল নং লিখুন">
+                            </div>
+                       
+    
+                        
+                            <div class="mb-3">
+                                <label for="message"><b>মেসেজ</b></label>
+                                 <textarea name="message" id="message" class="form-control rounded" rows="4"></textarea>
+                            </div>
+                         
+                         <div class="mb-3 text-center">
+                             <input type="submit" value="সাবমিট" class="btn">
+                         </div>
+                      </div>
                    
                 </div>
             </form>
